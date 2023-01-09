93863b04a9a0e46bd65f3a93411641f909f4ee00
Z. Michael Gehlke <zmgehlke@users.noreply.github.com>
2023-01-08 18:54:40 -0800

    Update index to fix rendering issue with images.

--- src/workflow/index_stats.py
+++ src/workflow/index_stats.py
@@ -1,3 +1,7 @@
+import pandas
+
+
+
 from .. import tools
 
 
@@ -15,18 +19,32 @@ def per_symbol_stats(index, datum, symbols, symbol_data, norm_idx=0):
     else:
         symbol_set = list(index["symbols"])
     
-    norm = 1 / len(symbol_set)
-    datum["nclose"] = symbol_data[symbol_set[0]]["nclose"] * norm
-    for symbol in symbol_set[1:]:
-        datum["nclose"] += symbol_data[symbol]["nclose"] * norm
+    set_data = []
+    for symbol in symbol_set:
+        symbol_extract = symbol_data[symbol][["date", "nclose"]]
+        symbol_extract["date"] = pandas.to_datetime(symbol_extract["date"])
+        symbol_extract.rename(columns={"nclose":f"{symbol}"}, inplace=True)
+        set_data.append(
+            symbol_extract
+        )
+    
+    joined_set_data = set_data[0]
+    joined_set_data["date"] = pandas.to_datetime(joined_set_data["date"])
+    for frame in set_data[1:]:
+        print(joined_set_data)
+        print(joined_set_data.dtypes)
+        joined_set_data = joined_set_data.merge(frame, on="date", how="outer", sort=True)
 
-    nclose_array = datum["nclose"].to_numpy(copy=True)
+    datum["avg_nclose"] = joined_set_data.mean(axis=1)
+
+
+    nclose_array = datum["avg_nclose"].to_numpy(copy=True)
     datum["nclose_EMA"] = tools.ema.spectrum(nclose_array, POWER).transpose().tolist()
 
     ### DIFFERENCES
     datum["nclose_diff"] = 0
     for idx in range(1, len(datum.index)):
-        datum["nclose_diff"][idx] = datum["nclose"][idx] - datum["nclose"][idx - 1]
+        datum["nclose_diff"][idx] = datum["avg_nclose"][idx] - datum["avg_nclose"][idx - 1]
     
     nclose_diff_array = datum["nclose_diff"].to_numpy(copy=True)
     datum["nclose_diff_EMA"] = tools.ema.spectrum(nclose_diff_array, POWER).transpose().tolist()
